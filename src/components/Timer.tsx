60ffc64d965124d96f51cd17c91483e0ea7c92a8
Franco Canzani <francocanzani@gmail.com>
2023-05-22 19:23:14 +0200

    Adds play, pause, reset button

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -6,29 +6,55 @@ function Countdown() {
     seconds: 0,
   });
 
+  const [isRunning, setIsRunning] = useState(false);
+
+  let interval = null;
+
   useEffect(() => {
-    const interval = setInterval(() => {
-      setCountdown((prevCountdown) => {
-        if (prevCountdown.seconds > 0) {
-          return {
-            ...prevCountdown,
-            seconds: prevCountdown.seconds - 1,
-          };
-        } else if (prevCountdown.minutes > 0) {
-          return {
-            minutes: prevCountdown.minutes - 1,
-            seconds: 59,
-          };
-        } else {
-          // Countdown is complete
-          clearInterval(interval);
-          return prevCountdown;
-        }
-      });
-    }, 1000);
+    if (isRunning) {
+      interval = setInterval(() => {
+        setCountdown((prevCountdown) => {
+          if (prevCountdown.seconds > 0) {
+            return {
+              ...prevCountdown,
+              seconds: prevCountdown.seconds - 1,
+            };
+          } else if (prevCountdown.minutes > 0) {
+            return {
+              minutes: prevCountdown.minutes - 1,
+              seconds: 59,
+            };
+          } else {
+            // Countdown is complete
+            clearInterval(interval);
+            return prevCountdown;
+          }
+        });
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
 
     return () => clearInterval(interval);
-  }, []);
+  }, [isRunning]);
+
+  function handleCountdown() {
+    setIsRunning(true);
+  }
+
+  function handleReset() {
+    clearInterval(interval);
+    setIsRunning(false);
+    setCountdown({
+      minutes: 25,
+      seconds: 0,
+    });
+  }
+
+  function handlePause() {
+    clearInterval(interval);
+    setIsRunning(false);
+  }
 
   return (
     <div className='text-white'>
@@ -36,10 +62,26 @@ function Countdown() {
         countdown.minutes
       }:${countdown.seconds.toString().padStart(2, '0')}`}</h1>
       <div className='flex items-center justify-center'>
-        <button className='m-6 rounded-md bg-slate-400 px-6 py-4 text-xl font-bold uppercase'>
-          Pause
-        </button>
-        <button className='m-6 rounded-md bg-red-400 px-6 py-4 text-xl font-bold uppercase'>
+        {isRunning ? (
+          <button
+            onClick={handlePause}
+            className='m-6 rounded-md bg-slate-400 px-6 py-4 text-xl font-bold uppercase active:translate-y-0.5'
+          >
+            Pause
+          </button>
+        ) : (
+          <button
+            onClick={handleCountdown}
+            className='m-6 rounded-md bg-green-400 px-6 py-4 text-xl font-bold uppercase active:translate-y-0.5'
+          >
+            Play
+          </button>
+        )}
+
+        <button
+          onClick={handleReset}
+          className='m-6 rounded-md bg-red-400 px-6 py-4 text-xl font-bold uppercase active:translate-y-0.5'
+        >
           Reset
         </button>
       </div>
