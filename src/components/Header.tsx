727d130ce56a365d8c84a210e3869eab347dff7f
Franco Canzani <francocanzani@gmail.com>
2023-05-26 23:40:21 +0200

    Fixed margins

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className='mt-8 flex items-center justify-center '>
+    <header className='mt-6 flex items-center justify-center '>
       <h1 className='px-2 text-5xl font-black uppercase sm:text-6xl'>
         Focus <span className='text-red-500'>Mate</span>
       </h1>
