a911fdcfdbd2eef1ce5d5c9f8bad61e96a893a64
Franco Canzani <francocanzani@gmail.com>
2023-05-28 00:20:53 +0200

    Adds vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+import { inject } from '@vercel/analytics';
+
+inject();
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
