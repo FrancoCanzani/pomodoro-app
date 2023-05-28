ab07cdba7a3b839f5d12198f17168edd37d0c7db
Franco Canzani <francocanzani@gmail.com>
2023-05-28 19:48:54 +0200

    Changed buttons from SVG to text for accesibility

--- src/App.tsx
+++ src/App.tsx
@@ -18,47 +18,6 @@ function App() {
 
   let interval: undefined | number = undefined;
 
-  const playSVG = (
-    <svg
-      xmlns='http://www.w3.org/2000/svg'
-      width='36'
-      height='36'
-      viewBox='0 0 24 24'
-    >
-      <path fill='currentColor' d='M8 19V5l11 7l-11 7Z' />
-    </svg>
-  );
-
-  const pauseSVG = (
-    <svg
-      xmlns='http://www.w3.org/2000/svg'
-      width='36'
-      height='36'
-      viewBox='0 0 24 24'
-    >
-      <path fill='currentColor' d='M14 19V5h4v14h-4Zm-8 0V5h4v14H6Z' />
-    </svg>
-  );
-
-  const resetSVG = (
-    <svg
-      xmlns='http://www.w3.org/2000/svg'
-      width='25'
-      height='25'
-      viewBox='0 0 14 14'
-    >
-      <g
-        fill='none'
-        stroke='currentColor'
-        stroke-linecap='round'
-        stroke-linejoin='round'
-      >
-        <path d='M7 13.5A6.5 6.5 0 1 1 13.5 7a7.23 7.23 0 0 1-2 5' />
-        <path d='m13.5 11.5l-2 .5l-.5-2M9 9L7 6.5H4' />
-      </g>
-    </svg>
-  );
-
   //   Play, pause and reset functions
   function handleCountdown() {
     setIsRunning(true);
